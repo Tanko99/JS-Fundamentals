fe492d0ff1f76cf20682506ed2843f0ce1f88ebc
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-07-12 21:50:37 +0100

    three languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,10 @@
+const Languages = ["C", "Python", "JavaScript"];
+Languages.forEach(Language =>{
+    if(Language === "C"){
+        console.log("C is fun") ;
+    } else if (Language === "Python"){
+        console.log("Python is cool");
+    } else if (Language === "JavaScript"){
+        console.log("JvaScript is amazing");
+    }
+});
